f15bc1bc0e198ae6de4daf9c93696a7dde252f79
nigama43 <88390007+nigama43@users.noreply.github.com>
2024-10-03 14:41:47 +0530

    Update Form.js

--- components/Form.js
+++ components/Form.js
@@ -1,1 +1,64 @@
+import React, { useState } from "react"; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min'; 
+import { useNavigate } from "react-router-dom";
+
+export default function Form() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate(); // Initialize useNavigate
+
+  const collectData = async (e) => {
+    e.preventDefault();
+    let result = await fetch('http://localhost:4000/', {
+      method: 'post',
+      body: JSON.stringify({ name, email, password }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    result = await result.json();
+    localStorage.setItem("user", JSON.stringify(result));
+
+    //navigate('/success'); // Redirect to the success page after submission
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={collectData}>
+        <h2 className="text-center pt-3">SIGNUP FORM</h2>
+        <div className="mb-3">
+          <label className="form-label">Username</label>
+          <input 
+            type="text" 
+            className="form-control" 
+            value={name} 
+            onChange={(e) => setName(e.target.value)} 
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Email Address</label>
+          <input 
+            type="email" 
+            className="form-control" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input 
+            type="password" 
+            className="form-control" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
 
