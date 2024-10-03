f8f107b6272252648c76a00dc44ba0809022d715
nigama43 <88390007+nigama43@users.noreply.github.com>
2024-10-03 14:40:19 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,24 @@
 
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'; // import Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min'; 
+import Form from './components/Form';
+import Success from './components/Success';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Form />} />
+          {/* <Route path="/success" element={<Success />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
