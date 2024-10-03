c4e9150179a408f98876706e1d7526738bf0a836
nigama43 <88390007+nigama43@users.noreply.github.com>
2024-10-03 14:42:10 +0530

    Update Success.js

--- components/Success.js
+++ components/Success.js
@@ -1,1 +1,14 @@
 
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Success = () => {
+  return (
+    <div>
+      <h1>Form is Submitted Successfully</h1>
+    </div>
+  );
+}
+
+export default Success;
